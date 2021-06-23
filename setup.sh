ee9f3890dd3838f1c0031ea179ee0da73a0f0ef9
NinjaPete <69259363+NinjaPete@users.noreply.github.com>
2021-06-23 10:24:25 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,33 @@
+sudo mkdir cep
+wget https://raw.githubusercontent.com/NinjaPete/cep/main/server.py
+cd cep
+sudo mkdir container
+sudo mkdir files
+sudo mkdir macro
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/UAC.msi
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/PotentiallyUnwanted.exe
+cd container
+wget https://github.com/NinjaPete/cep/blob/d5b33e999e5e139410553d657ecf3069029c18a3/TestContainer.dmg
+wget https://github.com/NinjaPete/cep/blob/d5b33e999e5e139410553d657ecf3069029c18a3/Test.tar
+wget https://github.com/NinjaPete/cep/blob/d5b33e999e5e139410553d657ecf3069029c18a3/Test.rar
+wget https://github.com/NinjaPete/cep/blob/d5b33e999e5e139410553d657ecf3069029c18a3/Test.7z
+wget https://github.com/NinjaPete/cep/blob/d5b33e999e5e139410553d657ecf3069029c18a3/Test.gz
+wget https://github.com/NinjaPete/cep/blob/d5b33e999e5e139410553d657ecf3069029c18a3/Test.zip
+cd ..
+cd files
+wget https://raw.githubusercontent.com/NinjaPete/cep/main/CEPlus.bat
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/CEPlusDMG.dmg
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/Test.dmg
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/Test.exe
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/Test.exe.lnk
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/Test.exe.pif
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/Test.jar
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/Test.msi
+wget https://raw.githubusercontent.com/NinjaPete/cep/main/Test.ps1
+wget https://raw.githubusercontent.com/NinjaPete/cep/main/Test.py
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/Test.scr
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/Test.sh
+cd ..
+cd macro
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/CEPlus.docm
+wget https://github.com/NinjaPete/cep/blob/17a8ccd0d1a18d25828586b783fccac5e0d3baa0/CEPlus.xlsm
